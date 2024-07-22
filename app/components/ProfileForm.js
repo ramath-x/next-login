88b817ee8f333e29bb674437dd7ee835b1f66217
ramath-x <114470428+ramath-x@users.noreply.github.com>
2024-07-22 22:19:42 +0700

    fornt-end -components RegisterForm

--- app/components/ProfileForm.js
+++ app/components/ProfileForm.js
@@ -11,14 +11,31 @@ export default function ProfileForm() {
                 </div>
                 <div className="card-body">
                     <div className="flex flex-row">
-                        <div>
-                            <label className="mt-2 mb-2">First Name</label>
-                            <input className="mt-2 mb-2 input input-bordered w-full max-w-xs" type="text" name="firstName" value="John"></input>
+                        <div className="mt-2 mb-2"> First Name
+                            <label className="mt-1 mb-1 mr-2 input input-bordered flex items-center gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    className="h-4 w-4 opacity-70">
+                                    <path
+                                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                </svg>
+                                <input type="text" className="grow" value={"John"} />
+                            </label>
                         </div>
-                        <div>
-
-                            <label className="mt-2 mb-2">Last Name</label>
-                            <input className="mt-2 mb-2 input input-bordered w-full max-w-xs" type="text" name="lastName" value="Doe"></input>
+                        <div className="mt-2 mb-2"> Last Name
+                            <label className="mt-1 mb-1 mr-2 input input-bordered flex items-center gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    className="h-4 w-4 opacity-70">
+                                    <path
+                                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                </svg>
+                                <input type="text" className="grow" value={"John"} />
+                            </label>
                         </div>
                     </div>
                     <div className="">
