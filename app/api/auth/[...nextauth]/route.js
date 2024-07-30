8736dabbbdcb8bf046784510e1077d71bea24fa8
ramath kunasan <ramath.x36@gmail.com>
2024-07-30 15:06:11 +0700

    แยก module

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -40,25 +40,7 @@ export const authOptions = {
         }
       },
     }),
-    CredentialsProvider({
-      id: "intranet-credentials",
-      name: "Two Factor Auth",
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "jsmith" },
-        two_fac_key: { label: "2FA Key" },
-      },
-      async authorize(credentials, req) {
-        if (!credentials) return null;
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-        // เช็ค 2FA จาก 2FA key ว่าตรงกันไหม
-        console.log("credentials", credentials);
-        console.log("user", user);
-        // return true;
-      },
-    }),
     GoogleProvider({
       name: "google",
       clientId: process.env.GOOGLE_CLIENT_ID,
