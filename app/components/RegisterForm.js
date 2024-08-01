e555f6ab9ef17e0dd73f62f4f95ca1e8ee396bb3
ramath kunasan <ramath.x36@gmail.com>
2024-08-01 15:47:03 +0700

    fron-end -Profile custom picture center -Register custom all design

--- app/components/RegisterForm.js
+++ app/components/RegisterForm.js
@@ -1,74 +1,91 @@
+"use client";
 export default function RegisterForm() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="card lg:card-side bg-base-100 shadow-xl">
-                <div className="mt-10 mb-2 text-3xl font-bold text-center text-gray-800">Register</div>
-                <div className="card-body">
-                    <div className="flex flex-row">
-                        <div className="mt-2 mb-2 mr-3"> First Name
-                            <label className="input input-bordered flex items-center gap-2">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 16 16"
-                                    fill="currentColor"
-                                    className="h-4 w-4 opacity-70">
-                                    <path
-                                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                                </svg>
-                                <input type="text" className="grow" placeholder="First Name" />
-                            </label>
-                        </div>
-                        <div className="mt-2 mb-2 mr-3"> Last Name
-                            <label className="input input-bordered flex items-center gap-2">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 16 16"
-                                    fill="currentColor"
-                                    className="h-4 w-4 opacity-70">
-                                    <path
-                                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                                </svg>
-                                <input type="text" className="grow" placeholder="Last Name" />
-                            </label>
-                        </div>
-                    </div>
-                    <div className="">
-                        Email
-                    </div>
-                    <label className="input input-bordered flex items-center gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                            <path
-                                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                        </svg>
-                        <input type="text" className="grow" placeholder="Enter your Email" />
-                    </label>
-                    <div>
-                        Password
-                    </div>
-                    <label className="input input-bordered flex items-center gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                fillRule="evenodd"
-                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                                clipRule="evenodd" />
-                        </svg>
-                        <input type="password" className="grow" placeholder="***********" />
-                    </label>
-                    <div className="flex flex-row-reverse">
-                        <button className="btn btn-primary">Edit Profile</button>
-                    </div>
-                </div>
+  const handleSubmit = () => {
+    console.log("register");
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <div className="card-body p-6">
+          <div className="mt-2 mb-2 text-3xl font-bold text-center text-gray-800">
+            Register
+          </div>
+          <div className="flex flex-row">
+            <div className="mt-2 mb-2 mr-3">
+              {" "}
+              First Name
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input type="text" className="grow" placeholder="First Name" />
+              </label>
             </div>
+            <div className="mt-2 mb-2 mr-3">
+              {" "}
+              Last Name
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+                <input type="text" className="grow" placeholder="Last Name" />
+              </label>
+            </div>
+          </div>
+          <div className="">Email</div>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input
+              type="text"
+              className="grow"
+              placeholder="Enter your Email"
+            />
+          </label>
+          <div>Password</div>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input type="password" className="grow" placeholder="***********" />
+          </label>
+          <div className="flex flex-row-reverse">
+            <button
+              className=" w-full bg-blue-500 text-white py-2 rounded"
+              onClick={handleSubmit}
+            >
+              Register
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
